0b6ed60e19fd7eb0dbec81f6eebb0abbf51da2be
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2022-02-13 21:28:09 +0100

    Methods of Unit-class implemented, w javaDoc, without handling exceptions

--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -32,4 +32,87 @@ public abstract class Unit {
         this.armor = armor;
     }
 
+    /**
+     * Attack method containing the formula given how the logic behind a unit's attack
+     * is in the game
+     * @param opponent, is the opponent that is being attacked
+     *                  The opponent's health is altered with it's own mutator-method
+     *                  setHealth
+     *                  The logic gets the pre-given health of the opponent,
+     *                  subtracts this unit's attack and attackBonus,
+     *                  additions the opponents armor and resistBonus,
+     *                  and then sets the result as the new health
+     *                  of the @param opponent
+     */
+    public void attack(Unit opponent){
+        opponent.setHealth(opponent.getHealth()
+                - (this.attack + this.getAttackBonus())
+                + (opponent.getArmor() + opponent.getResistBonus()));
+    }
+
+    /**
+     * Accessor method that returns the name of this Unit
+     * @return String name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Accessor method that returns the health of this Unit
+     * @return int health
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * Accessor method that returns the attack-quantity of this Unit
+     * @return int attack
+     */
+    public int getAttack() {
+        return attack;
+    }
+
+    /**
+     * Accessor method that returns the armor-quantity of this Unit
+     * @return int armor
+     */
+    public int getArmor() {
+        return armor;
+    }
+
+    /**
+     * Mutator method that alters the current health attribute of this Unit
+     * @param health, is a positive integer
+     */
+    public void setHealth(int health) {
+        //add exception, and param must be more than zero
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        //revise later
+        return "\nUNIT: \n" +
+                "Name = "   + name + "\n" +
+                "Health = " + health + "\n" +
+                "Attack = " + attack + "\n" +
+                "Armor = "  + armor;
+    }
+
+    /**
+     * Abstract method shell to force a unique attack-bonus method for each of
+     * the subclasses of Unit
+     * @return int attackBonus
+     */
+    public abstract int getAttackBonus();
+
+    /**
+     * Abstract method shell to force a unique resist-bonus method for each of
+     * the subclasses of Unit
+     * @return int resistBonus
+     */
+    public abstract int getResistBonus();
+
 }
