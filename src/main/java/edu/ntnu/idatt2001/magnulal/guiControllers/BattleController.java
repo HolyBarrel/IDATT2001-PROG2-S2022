6d90950baf1b39050da6fb8b750458fe25808e3a
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2022-05-19 10:46:36 +0200

    Created basic layout for 'battleScreen.fxml'

--- src/main/java/edu/ntnu/idatt2001/magnulal/guiControllers/BattleController.java
+++ src/main/java/edu/ntnu/idatt2001/magnulal/guiControllers/BattleController.java
@@ -1,9 +1,27 @@
 package edu.ntnu.idatt2001.magnulal.guiControllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class BattleController {
     @FXML
-    private Button btn;
+    private Button btnSkipToResults;
+    @FXML
+    private Button btnReturnHome;
+    @FXML
+    private Label exMsg;
+    @FXML
+    private Label nameArmy2;
+    @FXML
+    private Label nameArmy1;
+
+    @FXML
+    public void returnHome(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void skipToResults(ActionEvent actionEvent) {
+    }
 }
